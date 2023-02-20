9c14ef89263602dfb386f15dd59e88260aaa7a72
cess <me@ceseth.com>
2023-02-20 13:27:19 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+/**
+*main - Prints the alphabet lower case and upper case
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int n = 98;
+int m = 65;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+while (m <= 90)
+{
+putchar(m);
+m++;
+}
+putchar('\n');
+return (0);
+}
